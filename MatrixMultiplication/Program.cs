35514a3acab339550f1a8888404ca0cbc39e6bec
MARIA TERESA GUERRA ALVAREZ <mariaguerra145494@correo.itm.edu.co>
2026-08-30 11:26:22 -0500

    MatrixMultiplication finished

--- MatrixMultiplication/Program.cs
+++ MatrixMultiplication/Program.cs
@@ -5,9 +5,86 @@ var options = new List<string> { "s", "n" };
 
 do
 {
-    answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
-} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+    var m = ConsoleExtension.GetInt("Ingrese el valor de m: ");
+    var n = ConsoleExtension.GetInt("Ingrese el valor de n: ");
+    var p = ConsoleExtension.GetInt("Ingrese el valor de p: ");
 
-} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+    var matrixA = new int[m, n];
+    var matrixB = new int[n, p];
+    var matrixC = new int[m, p];
+
+    FillMatrixA(matrixA);
+    FillMatrixB(matrixB);
+    MultiplyMatrices(matrixA, matrixB, matrixC);
+
+    Console.WriteLine("*** A ***");
+    ShowMatrix(matrixA);
+
+    Console.WriteLine("*** B ***");
+    ShowMatrix(matrixB);
+
+    Console.WriteLine("*** C ***");
+    ShowMatrix(matrixC);
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+        Console.WriteLine();
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
 
 Console.WriteLine("Game Over.");
+
+void FillMatrixA(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = (i + 1) * j;
+        }
+    }
+}
+
+void FillMatrixB(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = (j + 1) * i;
+        }
+    }
+}
+
+void MultiplyMatrices(
+    int[,] matrixA,
+    int[,] matrixB,
+    int[,] matrixC)
+{
+    for (int i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixB.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrixA.GetLength(1); k++)
+            {
+                matrixC[i, j] +=
+                    matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
