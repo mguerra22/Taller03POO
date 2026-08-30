9433a5b9fae2dd85cd94619be34a2bd0004dcf75
MARIA TERESA GUERRA ALVAREZ <mariaguerra145494@correo.itm.edu.co>
2026-08-29 19:41:08 -0500

    In progress

---
+++ Rhombus/Program.cs
@@ -0,0 +1,13 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+
+Console.WriteLine("Game Over.");
